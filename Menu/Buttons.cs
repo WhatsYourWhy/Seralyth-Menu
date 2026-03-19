11570c73acf19bf05092285ccbd1aec0dec2661a
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-03-19 14:48:00 -0500

    fix grey screen gun

--- Menu/Buttons.cs
+++ Menu/Buttons.cs
@@ -1837,7 +1837,7 @@ namespace Seralyth.Menu
                 new ButtonInfo { buttonText = "Fix Screen Gun", aliases = new[] { "Ungrey Screen Gun" }, method = ()=> Overpowered.ActivateGreyZoneGun(false), toolTip = "Makes whoever your hand desires' screen normal again." },
                 new ButtonInfo { buttonText = "Grey Screen All", enableMethod = ()=> Overpowered.ActivateGreyZone(true), disableMethod =() => Overpowered.ActivateGreyZone(false), toolTip = "Makes everyone's screen grey." },
 
-                new ButtonInfo { buttonText = "Spaz Grey Screen Gun", method = Overpowered.SpazGreyZoneGun, disableMethod =() => SerializePatch.OverrideSerialization = null, toolTip = "Makes whoever your hand desires' screen flash grey." },
+                new ButtonInfo { buttonText = "Spaz Grey Screen Gun", method = Overpowered.SpazGreyZoneGun, disableMethod = () => SerializePatch.OverrideSerialization = null, toolTip = "Makes whoever your hand desires' screen flash grey." },
                 new ButtonInfo { buttonText = "Spaz Grey Screen All", method = Overpowered.SpazGreyZone, disableMethod =() => Overpowered.ActivateGreyZone(false), toolTip = "Makes everyone's screen flash grey." },
 
                 new ButtonInfo { buttonText = "Zero Gravity Gun", method = ()=> Overpowered.ActivateGreyZoneGun(true, true), toolTip = "Sets whoever your hand desires' gravity to zero." },
